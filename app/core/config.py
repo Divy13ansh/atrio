cc6e84041939513f0da815ce95da82ab0648f4e7
Divyansh <chawladivyansh131106@gmail.com>
2025-12-30 16:28:23 +0530

    setup config

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     ENV: str = "dev"
     PROJECT_NAME: str = "Angio AI"
+    DATABASE_URL: str
 
     DICOM_STORAGE_PATH: str = "storage/dicom"
     REPORT_STORAGE_PATH: str = "storage/reports"
